3186bfcebda511b0a63bd354fe1baed7a1092076
Yulkaz <yulkazs@outlook.com>
2025-04-18 00:19:44 +0200

    Small change: agenda

--- source/app/beheer/agenda/page.tsx
+++ source/app/beheer/agenda/page.tsx
@@ -243,7 +243,7 @@ export default function AgendaPage() {
   return (
     <div className="text-gray-800 p-4">
       <h2 className="text-xl sm:text-2xl font-bold mb-4 sm:mb-6 flex items-center gap-2">
-        <CalendarPlus size={20} className="sm:w-[24px] sm:h-[24px]" /> Agenda Beheer
+        <CalendarPlus size={20} className="sm:w-[24px] sm:h-[24px]" /> Agenda
       </h2>
       
       {error && (
