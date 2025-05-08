5908959c2eb7344414051a294d639bb00ff5f018
Yulkaz <yulkazs@outlook.com>
2025-05-08 19:10:42 +0200

    Testing size

--- source/app/fotoboek/page.tsx
+++ source/app/fotoboek/page.tsx
@@ -122,7 +122,7 @@ export default function Fotoboek() {
     
     return (
       <div 
-        className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50 p-4"
+        className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50 p-4 pt-20"
         onClick={() => setSelectedItem(null)}
       >
         <div 
